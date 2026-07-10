1964838eef492b2ebd34f30e8153a0026ecafb23
Nischal Singana <singananischal@gmail.com>
2026-07-11 05:07:12 +0530

    Revert UI components to original JSX to restore custom styling and fix IDE errors with ts-nocheck

--- my-app/src/components/ui/textarea.jsx
+++ my-app/src/components/ui/textarea.jsx
@@ -2,10 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Textarea = React.forwardRef<
-  HTMLTextAreaElement,
-  React.ComponentProps<"textarea">
->(({ className, ...props }, ref) => {
+const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
@@ -13,9 +10,8 @@ const Textarea = React.forwardRef<
         className
       )}
       ref={ref}
-      {...props}
-    />
-  )
+      {...props} />
+  );
 })
 Textarea.displayName = "Textarea"
 
